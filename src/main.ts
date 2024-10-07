25c571ac595df011c28651e9596aa164daa88e9c
romanmeclazcke1@gmail.com <romanmeclazcke1@gmail.com>
2024-10-06 21:44:30 -0300

    comment code to test

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,17 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalGuards()
-  app.enableCors({
-    origin: ['http://localhost:3000/'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    allowedHeaders: 'Content-Type, Accept',
-    exposedHeaders: 'Content-Range,X-Content-Range',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    maxAge: 3600,
-  });
+  //app.useGlobalGuards()
+  // app.enableCors({
+  //   origin: ['http://localhost:3000/'],
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   credentials: true,
+  //   allowedHeaders: 'Content-Type, Accept',
+  //   exposedHeaders: 'Content-Range,X-Content-Range',
+  //   preflightContinue: false,
+  //   optionsSuccessStatus: 204,
+  //   maxAge: 3600,
+  // });
 
   app.useGlobalPipes(
     new ValidationPipe({
